72b12146701af85d5a1744ccbaa5410467e53c52
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-18 20:51:31 -0300

    feat: update home page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,4 +1,8 @@
 import "./Home.css";
+import foto from "../../assets/images/foto8.png";
+import { RiWhatsappFill } from 'react-icons/ri';
+import { GoMarkGithub } from 'react-icons/go';
+import { HiOutlineMail } from 'react-icons/hi'
 
 export function Home() {
   return (
@@ -9,22 +13,24 @@ export function Home() {
             <li>Home</li>
             <li>Skills</li>
             <li>Portfolio</li>
-            <li>Contato</li>
           </ul>
         </header>
-        <div>
-          <img src="/" alt="img" />
+        <div className="div_img-data">
+            <div className="div_ctt">
+                <li className="li_whatssap"><a href="/" target="_blanc"><RiWhatsappFill/></a></li>
+                <li className="li_email"><a href="mailto:mateus_santiago2.3@outlook.com" target="_blanc"><HiOutlineMail/></a></li>
+                <li className="li_gitHub"><a href="https://github.com/MateusSantiagoDev" target="_blanc"><GoMarkGithub/></a></li>
+            </div>
+          <div className="div_img">
+            <img src={foto} alt="img" />
+          </div>
           <div className="div_data">
-            <h1>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
-              et.
-            </h1>
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Aspernatur officiis ad amet deleniti id. Nostrum facilis sapiente
-              dignissimos impedit atque?
-            </span>
-            <button className="button"></button>
+            <h3 className="h3_1">Web</h3>
+            <h1 className="h1">Developer</h1>
+            <h3 className="h3_2">Full Stack</h3>
+            <div className="div_button">
+            <button className="button">Sobre mim</button>
+            </div>
           </div>
         </div>
       </div>
