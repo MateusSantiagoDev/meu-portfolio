506ef616d6285c46b4829186d101831c284ab7a3
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-19 16:55:06 -0300

    feat(portfolio): implemented media query

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -23,14 +23,14 @@ export function Portfolio() {
             97779-8990 - Rua Lucilo Varejão 31 - Jardim Cliper - São Paulo - SP
             - CEP: 04827-240 E-mail: Mateus_santiago2.3@outlook.com
           </span>
-          <h3>Experiência de trabalho</h3>
-          <span>
+          <h3 className="trabalho">Experiência de trabalho</h3>
+          <span className="trabalho">
             <b>Hagana tecnologia comercio de sistemas LTDA</b> período: 4 anos
             e 11 meses | Funções: porteiro de edificios - 2 anos e 6 meses |
             Monitor de sistemas eletrônicos: 2 anos e 4 meses | auxiliar de
             escritório: 1 mês <br/>
           </span>
-          <span>
+          <span className="trabalho">
             <b>No Risk Sistemas de Seg e Log LTDA</b> período: 1 ano e 7 meses
             | Função: Operador de Monitoramento <br/>
           </span>
